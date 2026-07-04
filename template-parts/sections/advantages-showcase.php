e00a069afc33e5dd34ad4f9bcb5e2840e1c245dc
kotov-sergey <kotovsergey.dev@gmail.com>
2026-07-04 20:46:34 +0300

    feat(section-advantages): created a basic benefits section for the builder

---
+++ template-parts/sections/advantages-showcase.php
@@ -0,0 +1,66 @@
+<?php
+// Секция: Наши преимущества
+
+$section_title = get_sub_field( 'section_title' );
+$section_subtitle = get_sub_field( 'section_subtitle' );
+?>
+
+<section class="section advantages-showcase section--white">
+    <div class="container">
+
+        <?php if ( $section_title ) : ?>
+            <div class="advantages-showcase__header">
+                
+                <h2 class="advantages-showcase__title"><?php echo esc_html( $section_title ); ?></h2>
+                
+                <?php if ( $section_subtitle ) : ?>
+                    <p class="advantages-showcase__subtitle"><?php echo esc_html( $section_subtitle ); ?></p>
+                <?php endif; ?>
+
+            </div>
+        <?php endif; ?>
+
+        
+        <?php if ( have_rows( 'section_repeater' ) ) : ?>
+
+            <div class="l-grid l-grid--3">
+
+                <?php while ( have_rows( 'section_repeater' ) ) : the_row();
+
+                    $repeater_icon = get_sub_field( 'repeater_icon' );
+                    $repeater_title = get_sub_field( 'repeater_title' );
+                    $repeater_description = get_sub_field( 'repeater_description' );
+                ?>
+
+                <article class="advantage-card">
+
+                    <?php if ( $repeater_icon ) : ?>
+                        <div class="advantage-card__media">
+                            <?php echo wp_get_attachment_image( $repeater_icon, 'medium' ); ?>
+                        </div>
+                    <?php endif; ?>
+
+                    <div class="advantage-card__content">
+                        <?php if ( $repeater_title ) : ?>
+                            <h3 class="advantage-card__title"><?php echo esc_html( $repeater_title ); ?></h3>
+                        <?php endif; ?>
+
+                        <?php if ( $repeater_description ) : ?>
+                            <p class="advantage-card__description"><?php echo esc_html( $repeater_description ); ?></p>
+                        <?php endif; ?>
+                    </div>
+
+                </article>
+
+                <?php endwhile; ?>
+
+            </div>
+
+        <?php else : ?>
+
+            <p class="text-mute">Пока не было добавлено карточек преимуществ.</p>
+        
+        <?php endif; ?>
+
+    </div>
+</section>
