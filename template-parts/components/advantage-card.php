11ecebe7071a81acd02c01fa6445b4e52b7682dc
kotov-sergey <kotovsergey.dev@gmail.com>
2026-07-05 15:33:30 +0300

    feat(advantage-card): created a separate benefit card component

---
+++ template-parts/components/advantage-card.php
@@ -0,0 +1,27 @@
+<?php
+// Карточка преимуществ
+
+$card_icon = $args['card_icon'] ?? '';
+$card_title = $args['card_title'] ?? 'Заголовок карточки';
+$card_description = $args['card_description'] ?? 'Описание карточки';
+?>
+
+<article class="advantage-card">
+
+    <?php if ( $card_icon ) : ?>
+        <div class="advantage-card__media">
+            <?php echo airliner_get_svg( 'advantages/' . $card_icon ); ?>
+        </div>
+    <?php endif; ?>
+
+    <div class="advantage-card__content">
+        <?php if ( $card_title ) : ?>
+            <h3 class="advantage-card__title"><?php echo esc_html( $card_title ); ?></h3>
+        <?php endif; ?>
+
+        <?php if ( $card_description ) : ?>
+            <p class="advantage-card__description"><?php echo esc_html( $card_description ); ?></p>
+        <?php endif; ?>
+    </div>
+
+</article>
