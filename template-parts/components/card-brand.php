3816dedc299dde939622e6a4eeb98ae3165a450d
kotov-sergey <kotovsergey.dev@gmail.com>
2026-07-23 20:57:07 +0300

    fix(brands): fixed an incorrect link in the brand component and made the component secure

--- template-parts/components/card-brand.php
+++ template-parts/components/card-brand.php
@@ -6,30 +6,33 @@ $brand = $args['current_brand'] ?? null;
 
 if ( ! $brand ) return;
 
+$brand_link = get_term_link( $brand );
+if ( is_wp_error( $brand_link ) ) return;
+
 $logo = get_field( 'brand_logo', $brand );
 $brand_country = get_field( 'brand_country', $brand );
 ?>
 
-<a href="<?php echo esc_url( $brand->link ); ?>" class="card-brand">
-
+<a href="<?php echo esc_url( $brand_link ); ?>" class="card-brand">
 	<div class="card-brand__picture">
 		
 		<!-- Логотип производителя -->
 		<?php if ( $logo ) : ?>
 			<img 
-				 src="<?php echo esc_url( $logo['url'] ); ?>" 
-				 class="card-brand__image" 
-				 alt="Логотип производителя <?php echo esc_attr( $brand->name ); ?>" 
-				 loading="lazy" 
+				src="<?php echo esc_url( $logo['url'] ); ?>" 
+				class="card-brand__image" 
+				alt="Логотип производителя <?php echo esc_attr( $brand->name ); ?>" 
+				loading="lazy" 
 			/>
 		<?php else : ?>
 			<img 
-				 src="<?php echo esc_url( get_template_directory_uri() . '/public/images/placeholder-image.svg' ); ?>" 
-				 class="card-brand__image" 
-				 alt="Логотип производителя <?php echo esc_attr( $brand->name ); ?>" 
-				 loading="lazy" 
+				src="<?php echo esc_url( get_template_directory_uri() . '/public/images/placeholder-image.svg' ); ?>" 
+				class="card-brand__image" 
+				alt="Логотип производителя <?php echo esc_attr( $brand->name ); ?>" 
+				loading="lazy" 
 			/>
 		<?php endif; ?>
+
 	</div>
 
 	<div class="card-brand__body">
@@ -41,30 +44,21 @@ $brand_country = get_field( 'brand_country', $brand );
 		<div class="card-brand__meta">
 			
 			<!-- Страна производителя -->
-			<?php if ( $brand_country ) : ?>
+			<?php if ( ! empty ( $brand_country ) )  : ?>
 				<span class="card-brand__country">
 					<?php echo esc_html( $brand_country->name ); ?>
 				</span>
 			<?php endif; ?>
 			
 			<!-- Кол-во моделей -->
-			<?php if ( $brand->count > 0 ) : ?>
-				<span class="card-brand__count">
-					<?php 
-						echo $brand->count . ' '; 
-						echo my_declension( $brand->count, array('модель', 'модели', 'моделей' ) ); 
-					?>
-				</span>
-			<?php else : ?>
-				<span class="card-brand__count">
-					<?php 
-						echo $brand->count . ' '; 
-						echo my_declension( $brand->count, array('модель', 'модели', 'моделей' ) ); 
-					?>
-				</span>
-			<?php endif; ?>
+			<span class="card-brand__count">
+				<?php 
+					echo $brand->count . ' '; 
+					echo my_declension( $brand->count, array('модель', 'модели', 'моделей' ) ); 
+				?>
+			</span>
+
 		</div>
 
 	</div>
-
 </a>
--- template-parts/sections/brands.php
+++ template-parts/sections/brands.php
@@ -1,10 +1,11 @@
 <?php
-
 // Верстка секции производителей
 
 $brands = get_sub_field( 'brands' );
 
 $section_header = airliner_prepare_header_args( get_sub_field( 'section_header' ), $args );
+
+if ( empty( $brands ) ) return;
 ?>
 
 <!-- Секция популярные производители -->
@@ -13,24 +14,15 @@ $section_header = airliner_prepare_header_args( get_sub_field( 'section_header' 
 		
 		<?php get_template_part( 'template-parts/components/section-header', null, $section_header ); ?>
 
-		<?php 
-    
-        if ( $brands ) : 
-        ?>
-		
-        <ul class="l-grid brands-grid">
+		<ul class="l-grid brands-grid">
 			
 			<?php foreach ($brands as $brand) : ?>
-	
 				<li class="brands-grid__item">
 					<?php get_template_part( 'template-parts/components/card-brand', null, array( 'current_brand' => $brand ) ); ?>
 				</li>
-			
 			<?php endforeach; ?>
-        
-		</ul>
 		
-		<?php endif; ?>
+		</ul>
 		
     </div>
 </section>
