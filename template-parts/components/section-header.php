49b86fd9b78cee89ca69ce661f904a29e7e4dd44
kotov-sergey <kotovsergey.dev@gmail.com>
2026-07-27 20:44:33 +0300

    fix(section-header): added logic for displaying meta-information in the section header component #2

--- template-parts/components/section-header.php
+++ template-parts/components/section-header.php
@@ -12,7 +12,7 @@ $section_label = $header_data['header_label'] ?? '';
 $section_title = $header_data['header_title'];
 $section_description = $header_data['header_description'] ?? '';
 
-$section_alignment = $header_data['header_alignment'] ?: 'left';
+$section_alignment = $header_data['header_alignment'] ?: 'row';
 $section_tag = $header_data['header_tag'] ?: 'h2';
 
 $section_modifier = 'section-header--' . $section_alignment;
