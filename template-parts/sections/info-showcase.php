ea7ad9a796552ae815d6ffad6cacb0c32cbf43af
kotov-sergey <kotovsergey.dev@gmail.com>
2026-09-02 17:34:19 +0300

    style(info-showcase): added responsiveness to the infographic section

--- template-parts/sections/info-showcase.php
+++ template-parts/sections/info-showcase.php
@@ -6,6 +6,10 @@ $section_header = get_sub_field( 'section_header' );
 
 $section_description = get_sub_field( 'section_description' );
 
+if ( ! $section_header && ! $section_description && ! have_rows( 'section_cards' ) ) {
+    return;
+}
+
 $section_background = get_sub_field( 'section_background' );
 
 if ( $section_background === 'default' || ! $section_background ) {
@@ -18,11 +22,13 @@ $classes = trim( 'section info-showcase ' . $section_background );
 <section class="<?php echo esc_attr( $classes ); ?>">
     <div class="container">
 
+        <!-- Сетка разделения контента -->
         <div class="info-showcase__grid">
 
             <?php if ( $section_header || $section_description ) : ?>
                 <div class="info-showcase__content">
 
+                    <!-- Заголовок секции -->
                     <?php
                         get_template_part( 'template-parts/components/section-header', null, [
                             'data' => $section_header,
@@ -30,15 +36,18 @@ $classes = trim( 'section info-showcase ' . $section_background );
                         ] );
                     ?>
 
+                    <!-- Описание секции -->
                     <div class="info-showcase__description entry-content"><?php echo wp_kses_post( wpautop( $section_description ) ); ?></div>
 
                 </div>
             <?php endif; ?>
 
+            <!-- Блок с карточками преимуществ -->
             <?php if ( have_rows( 'section_cards' ) ) : ?>
                 <div class="info-showcase__infographic">
                     <?php while ( have_rows( 'section_cards') ) : the_row(); ?>
 
+                        <!-- Карточка преимуществ -->
                         <?php 
                             get_template_part( 'template-parts/components/card-advantage', null, [
                                 'card_icon' => get_sub_field( 'card_icon' ),
