bf1bd88ea3153d4b0649ef04ab93b4f4f660f14f
kotov-sergey <kotovsergey.dev@gmail.com>
2026-09-16 01:13:50 +0300

    feat(card-fuselage): updated the layout and styles of the fuselage card

--- template-parts/components/card-fuselage.php
+++ template-parts/components/card-fuselage.php
@@ -1,68 +1,107 @@
 <?php
 // Карточка типа фюзеляжа
 
+// Получение текущего типа фюзеляжа
 $fuselage_type = $args['current_type'] ?? null;
 if ( ! $fuselage_type ) return;
 
-
+// Ссылка на тип фюзеляжа
 $fuselage_type_link = get_term_link( $fuselage_type );
 if ( is_wp_error( $fuselage_type_link ) ) return;
 
+// Данные карточки типа фюзеляжа
 $label = get_field( 'label', $fuselage_type);
 $image = get_field( 'image', $fuselage_type);
 
+// Характеристики конкретного типа фюзеляжа
 $planes = get_field( 'examples', $fuselage_type );
 $capacity = get_field( 'capacity', $fuselage_type );
 $range = get_field( 'range', $fuselage_type );
+
+// ALT-аттрибут для изображения
+$alt_text = $fuselage_type->name . ' самолёты';
+
+// Путь к картинке-заглушке
+$placeholder = get_template_directory_uri() . '/public/images/placeholder-image.svg';
 ?>
 
-<div class="card-fuselage">
+<article class="card-fuselage">
+
+    <!-- Блок с изображение и подписью карточки -->
     <div class="card-fuselage__picture">
 
+        <!-- Изображение карточки -->
         <?php if ( $image ) : ?>
-            <?php echo wp_get_attachment_image( $image, 'large', false, ['class' => 'card-fuselage__image'] ); ?>
+            <?php echo wp_get_attachment_image( $image, 'large', false, ['class' => 'card-fuselage__image', 'alt' => $alt_text] ); ?>
+        <?php else : ?>
+            <img src="<?php echo esc_url( $placeholder ); ?>" class="card-fuselage__image" alt="<?php echo esc_attr( $alt_text ); ?>" />
         <?php endif; ?>
 
+        <!-- Подпись карточки -->
         <?php if ( $label ) : ?>
-            <span class="pill pill--sm pill--subtle card-fuselage__badge"><?php echo esc_html( $label ); ?></span>
+            <span class="pill pill--sm pill--subtle card-fuselage__label"><?php echo esc_html( $label ); ?></span>
         <?php endif; ?>
 
     </div>
 
     <div class="card-fuselage__body">
-        <a href="<?php echo esc_url( $fuselage_type_link ); ?>" class="card-fuselage__link" aria-label="Перейти на категорию: <?php echo esc_attr( $fuselage_type->name ); ?>">
-            <h3 class="card-fuselage__title">
+
+        <!-- Заголовок карточки -->
+        <h3 class="card-fuselage__title">
+            <a href="<?php echo esc_url( $fuselage_type_link ); ?>" class="card-fuselage__link" aria-label="Перейти на категорию: <?php echo esc_attr( $fuselage_type->name ); ?>">
                 <?php echo esc_html( $fuselage_type->name ); ?>
-            </h3>
-        </a>
+            </a>
+        </h3>
 
-        <p class="card-fuselage__description">
+        <!-- Описание карточки -->
+        <div class="card-fuselage__description">
             <?php echo wp_kses_post( $fuselage_type->description ); ?>
-        </p>
+        </div>
 
-        <dl class="card-fuselage__specs">
+        <!-- Характеристики карточки -->
+        <?php if ( $capacity || $range ) : ?>
             
-            <?php if ( $capacity ) : ?>
-                <div class="card-fuselage__spec-row">
-                    <dt class="card-fuselage__spec-row-label">Вместимость</dt>
-                    <dd class="card-fuselage__spec-row-value"><?php echo esc_html( $capacity ); ?></dd>
-                </div>
-            <?php endif; ?>
+            <div class="card-fuselage__specs">
+                
+                <!-- Вместимость -->
+                <?php if ( $capacity ) : ?>
 
-             <?php if ( $range ) : ?>
-                <div class="card-fuselage__spec-row">
-                    <dt class="card-fuselage__spec-row-label">Дальность</dt>
-                    <dd class="card-fuselage__spec-row-value"><?php echo esc_html( $range ); ?></dd>
-                </div>
-            <?php endif; ?>           
+                    <div class="spec-row spec-row--clean-icon">
+                        <div class="spec-row__name">
+                            <span class="spec-row__label">Вместимость</span>
+                        </div>
+                        <div class="spec-row__data">
+                            <span class="spec-row__value"><?php echo esc_html( $capacity ); ?></span>
+                        </div>
+                    </div>
+                    
+                <?php endif; ?>
 
-        </dl>
+                <!-- Дальность -->
+                 <?php if ( $range ) : ?>
 
+                    <div class="spec-row spec-row--clean-icon">
+                        <div class="spec-row__name">
+                            <span class="spec-row__label">Дальность</span>
+                        </div>
+                        <div class="spec-row__data">
+                            <span class="spec-row__value"><?php echo esc_html( $range ); ?></span>
+                        </div>
+                    </div>
+
+                <?php endif; ?>           
+
+            </div>
+
+        <?php endif; ?>
+
+        <!-- Примеры авиалайнеров -->
         <?php if ( $planes ) : ?>
             <div class="card-fuselage__examples">
 
-                <span class="card-fuselage__examples-title">Примеры</span>
+                <span class="card-fuselage__subtitle">Примеры</span>
 
+                <!-- Тэги для примеров авилайнеров -->
                 <div class="card-fuselage__tags">
                     
                     <?php foreach ( $planes as $plane ) :
@@ -72,22 +111,27 @@ $range = get_field( 'range', $fuselage_type );
 
                         <?php if ( $plane_link && ! is_wp_error( $plane_link ) ) : ?>
 
-                            <a href="<?php echo esc_url( $plane_link ); ?>">
-                                <span class="pill pill--sm pill--subtle card-fuselage__tag"><?php echo esc_html( $plane_title ); ?></span>
+                            <!-- Тэг-пример авиалайнера -->
+                            <a href="<?php echo esc_url( $plane_link ); ?>" class="pill pill--sm pill--subtle card-fuselage__tag">
+                                <?php echo esc_html( $plane_title ); ?>
                             </a>
 
                         <?php else : ?>
                             
-                            <span class="pill pill--sm pill--subtle card-fuselage__tag"><?php echo esc_html( $plane_title ); ?></span>
+                            <!-- Текст-заглушка авиалайнера -->
+                            <span class="pill pill--sm pill--subtle card-fuselage__tag">
+                                <?php echo esc_html( $plane_title ); ?>
+                            </span>
 
                         <?php endif; ?>
 
                     <?php endforeach; ?>
 
                 </div>
+
             </div>
-            
         <?php endif; ?>
 
     </div>
-</div>
+
+</article>
