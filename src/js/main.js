39b9fc215a0f74679ab70fc6bef5353d19761b64
kotov-sergey <kotovsergey.dev@gmail.com>
2026-09-16 20:53:05 +0300

    fix(main.js): updated main.js and moved all modules to a separate folder

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,8 @@
 // Импорт стилей (Vite сам выделит их в отдельный CSS файл при билде)
 import '../scss/main.scss';
 
-// Скрипты бургер-меню
-import './burger.js';
-
-// Скрипты кнопки hero-секции
-import './scroll.js';
-
-// Скрипты подключения галереи Glightbox
-import './gallery.js';
-
-// Скрипты для подключения AJAX-фильтров
-import './filter.js';
-
-// Скрипты для аккардеона блоков фильтрации
-import './catalog-filter-accordion.js';
-
-// Твой JS код
-console.log('Vite подключен к WordPress!');
+import './modules/burger.js'; // Скрипты бургер-меню
+import './modules/scroll.js'; // Скрипты кнопки hero-секции
+import './modules/gallery.js'; // Скрипты подключения галереи Glightbox
+import './modules/filter.js'; // Скрипты для подключения AJAX-фильтров
+import './modules/catalog-filter-accordion.js'; // Скрипты для аккардеона блоков фильтрации
